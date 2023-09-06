ec0df8ed7535ed40873ba1f1cb9bf2c755e8ce72
優菜 廣田 <y_hirota@tozan.co.jp>
2023-09-06 09:43:52 +0900

    プロジェクト ファイルを追加します。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tec_empty_box_supply_transport_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
