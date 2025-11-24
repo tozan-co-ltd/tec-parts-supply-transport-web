956c4cf48dd9dac4dbb2b5795673e5884b04cd33
sonn <Son_ND@DSP41P>
2025-11-24 11:49:09 +0900

    PreparationModel とTransportationModelを修正

--- Models/PreparationModel.cs
+++ Models/PreparationModel.cs
@@ -11,11 +11,17 @@ namespace tec_parts_supply_transport_web.Models
         [Column("machine_num")]
         public string MachineNum { get; set; }
 
+        [Column("product_num")]
+        public string ProductNum { get; set; }
+
         [Column("permanent_abbreviation")]
         public string PermanentAbbreviation { get; set; }
 
         [Column("box_type")]
         public string BoxType { get; set; }
+
+        [Column("issue_branch_num")]
+        public int IssueBranchNum { get; set; }
 
         [Column("box_count")]
         public int BoxCount { get; set; }
@@ -29,16 +35,31 @@ namespace tec_parts_supply_transport_web.Models
         [Column("ready_IPaddress")]
         public string ReadyIPaddress { get; set; }
 
+        [Column("transportation_IPaddress")]
+        public string TransportationIPaddress { get; set; }
+
         [Column("corrected_request_datetime")]
         public DateTime CorrectedRequestDatetime { get; set; }
 
+        [Column("transportation_start_datetime")]
+        public DateTime TransportationStartDatetime { get; set; }
+
+        [Column("transportation_end_datetime")]
+        public DateTime TransportationEndDatetime { get; set; }
+
         [Column("is_express")]
         public int IsExpress { get; set; }
+
+        [Column("is_completed")]
+        public int IsCompleted { get; set; }
 
         [Column("is_deleted")]
         public int IsDeleted { get; set; }
 
         [Column("empty_box_supply_status_id")]
         public int EmptyBoxSupplyStatusId { get; set; }
+
+        [Column("request_device_name")]
+        public string RequestDeviceName { get; set; }
     }
 }
--- Models/TransportationModel.cs
+++ Models/TransportationModel.cs
@@ -11,8 +11,14 @@ namespace tec_parts_supply_transport_web.Models
         [Column("machine_num")]
         public string MachineNum { get; set; }
 
+        [Column("product_num")]
+        public string ProductNum { get; set; }
+
         [Column("permanent_abbreviation")]
         public string PermanentAbbreviation { get; set; }
+
+        [Column("issue_branch_num")]
+        public int IssueBranchNum { get; set; }
 
         [Column("box_type")]
         public string BoxType { get; set; }
@@ -27,18 +33,33 @@ namespace tec_parts_supply_transport_web.Models
         public string TransportationIPaddress { get; set; }
 
         [Column("ready_datetime")]
-        public DateTime ReadyDatetime { get; set; } 
-        
+        public DateTime ReadyDatetime { get; set; }
+
+        [Column("ready_IPaddress")]
+        public string ReadyIPaddress { get; set; }
+
         [Column("corrected_request_datetime")]
         public DateTime CorrectedRequestDatetime { get; set; }
 
+        [Column("transportation_start_datetime")]
+        public DateTime TransportationStartDatetime { get; set; }
+
+        [Column("transportation_end_datetime")]
+        public DateTime TransportationEndDatetime { get; set; }
+
         [Column("is_express")]
         public int IsExpress { get; set; }
+
+        [Column("is_completed")]
+        public int IsCompleted { get; set; }
 
         [Column("is_deleted")]
         public int IsDeleted { get; set; }
         
         [Column("empty_box_supply_status_id")]
         public int EmptyBoxSupplyStatusId { get; set; }
+
+        [Column("request_device_name")]
+        public string RequestDeviceName { get; set; }
     }
 }
